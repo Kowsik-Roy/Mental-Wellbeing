1b2d0a04187b56f4e9f5aec9a5da667da6870b7c
Kowsik-Roy <kowsikroy2002@gmail.com>
2026-01-01 22:40:08 +0600

    Remove unnecessary files and Vercel config, add SQL backups to gitignore

--- test_controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-
-use App\Http\Controllers\HabitController;
-
-echo "HabitController exists: " . (class_exists(HabitController::class) ? 'YES' : 'NO') . "\n";
-echo "File exists: " . (file_exists(__DIR__.'/app/Http/Controllers/HabitController.php') ? 'YES' : 'NO') . "\n";
